7c115bf28dc85a4fbfe9be1df35296b12f4101ee
macrespo42 <macrespo@student.42.fr>
2024-08-02 15:48:41 +0200

    chore: ignore generated datasets

--- multilayer_perceptron/data_engineering/__init__.py
+++ multilayer_perceptron/data_engineering/__init__.py
@@ -1,5 +1,6 @@
 """Data engineering module."""
 
 from .parse import read_csv_with_WDBC_headers, load
+from .separate import separate
 
-__all__ = ["read_csv_with_WDBC_headers", "load"]
+__all__ = ["read_csv_with_WDBC_headers", "load", "separate"]
