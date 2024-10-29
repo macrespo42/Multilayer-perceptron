f0a935a76099230f06db6fd6730fd64b7581ade1
macrespo42 <macrespo@student.42.fr>
2024-10-29 15:31:24 +0100

    feat: add extension to splitted data

--- multilayer_perceptron/data_engineering/separate.py
+++ multilayer_perceptron/data_engineering/separate.py
@@ -2,6 +2,7 @@
 
 import math
 import sys
+
 from parse import read_csv_with_WDBC_headers
 
 
@@ -18,8 +19,8 @@ def separate(path: str, name="data", train_percentage=80) -> None:
     data_train = df.iloc[0:train_stop_idx]
     data_test = df.iloc[train_stop_idx::]
 
-    train_path = f"datasets/{name}_train"
-    test_path = f"datasets/{name}_test"
+    train_path = f"datasets/{name}_train.csv"
+    test_path = f"datasets/{name}_test.csv"
 
     try:
         data_train.to_csv(train_path)
